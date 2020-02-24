f6d5f2c140cdc7a289f1aa3bd33043c7a705c603
Richard Mario <richard.mario@lottemart.co.id>
2020-02-24 18:03:09 +0700

    second commit

--- lib/pages/mainMenu.dart
+++ lib/pages/mainMenu.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:m_coop/models/members.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class MainMenu extends StatefulWidget {
   @override
@@ -8,7 +9,10 @@ class MainMenu extends StatefulWidget {
 }
 
 class _MainMenuState extends State < MainMenu > {
-  final GlobalKey <ScaffoldState> keyScaffold = GlobalKey<ScaffoldState>();
+  final GlobalKey < ScaffoldState > keyScaffold = GlobalKey < ScaffoldState > ();
+  TextEditingController _emailController = new TextEditingController();
+  TextEditingController _passwordController = new TextEditingController();
+  TextEditingController _nameController = new TextEditingController();
   final List < Member > lstMember = [
     Member('Ubaidillah', '312312312', 'Jakarta,31 -02 - 1978', 'Ubaidillah@gmail.com', 'Pria', '08112322234', 'Kampung Karang Kendal', 1),
     Member('Ubaidillah', '312312312', 'Jakarta,31 -02 - 1978', 'Ubaidillah@gmail.com', 'Pria', '08112322234', 'Kampung Karang Kendal', 0),
@@ -24,6 +28,107 @@ class _MainMenuState extends State < MainMenu > {
     Member('Ubaidillah', '312312312', 'Jakarta,31 -02 - 1978', 'Ubaidillah@gmail.com', 'Pria', '08112322234', 'Kampung Karang Kendal', 1),
   ];
 
+  Widget _input(Icon icon, String hint, TextEditingController controller,
+    bool obsecure) {
+    return Container(
+      padding: EdgeInsets.only(left: 20, right: 20),
+      child: TextField(
+        controller: controller,
+        obscureText: obsecure,
+        style: TextStyle(
+          fontSize: 20,
+        ),
+        decoration: InputDecoration(
+          hintStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          hintText: hint,
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30),
+            borderSide: BorderSide(
+              color: Theme.of(context).primaryColor,
+              width: 2,
+            ),
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30),
+            borderSide: BorderSide(
+              color: Theme.of(context).primaryColor,
+              width: 3,
+            ),
+          ),
+          prefixIcon: Padding(
+            child: IconTheme(
+              data: IconThemeData(color: Theme.of(context).primaryColor),
+              child: icon,
+            ),
+            padding: EdgeInsets.only(left: 30, right: 10),
+          )),
+      ),
+    );
+  }
+  Widget _button(String text, Color splashColor, Color highlightColor,
+    Color fillColor, Color textColor, void
+    function ()) {
+    return RaisedButton(
+      highlightElevation: 0.0,
+      splashColor: splashColor,
+      highlightColor: highlightColor,
+      elevation: 0.0,
+      color: fillColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: new BorderRadius.circular(30.0)),
+      child: Text(
+        text,
+        style: TextStyle(
+          fontWeight: FontWeight.bold, color: textColor, fontSize: 20),
+      ),
+      onPressed: () {
+        function ();
+      },
+    );
+  }
+  Widget _panel(ScrollController sc) {
+    return MediaQuery.removePadding(
+      context: context,
+      removeTop: true,
+      child: ListView(
+        controller: sc,
+        children: < Widget > [
+          SizedBox(height: 12.0, ),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: < Widget > [
+              Container(
+                width: 30,
+                height: 5,
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.all(Radius.circular(12.0))
+                ),
+              ),
+            ],
+          ),
+
+          SizedBox(height: 18.0, ),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: < Widget > [
+              Text(
+                "Add New Data",
+                style: TextStyle(
+                  fontWeight: FontWeight.normal,
+                  fontSize: 24.0,
+                  color: Theme.of(context).primaryColor
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 36.0, ),
+        ],
+      )
+    );
+  }
   @override
   Widget build(BuildContext context) {
     _cardItem(Member member) {
@@ -67,42 +172,53 @@ class _MainMenuState extends State < MainMenu > {
       key: keyScaffold,
       extendBody: true,
       backgroundColor: Theme.of(context).primaryColor,
-      body: Container(
-        padding: EdgeInsets.all(8),
-        child: ListView.builder(
-          itemBuilder: (ctx, idx) {
-            return _cardItem(lstMember[idx]);
-          },
-          itemCount: lstMember.length,
-          scrollDirection: Axis.vertical,
-          shrinkWrap: true,
-        ),
-      ),
-      bottomNavigationBar: Container(
-        height: 55,
-        child: BottomAppBar(
-          color: Theme.of(context).primaryColor,
-          shape: CircularNotchedRectangle(),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: < Widget > [
-              IconButton(
-                iconSize: 22,
-                padding: EdgeInsets.only(left: 20),
-                color: Colors.white,
-                icon: Icon(Icons.arrow_back_ios), onPressed: () {
-                  Navigator.of(context).pop();
-                }),
-            ],
+      body: Stack(
+        alignment: Alignment.topCenter,
+        children: < Widget > [
+
+          SlidingUpPanel(
+            maxHeight: MediaQuery.of(context).size.height * 0.8,
+            minHeight: 20,
+            parallaxEnabled: true,
+            parallaxOffset: 1,
+            borderRadius: BorderRadius.only(topLeft: Radius.circular(18.0), topRight: Radius.circular(18.0)),
+            panelBuilder: (sc) => _panel(sc),
+            body: Container(
+              padding: EdgeInsets.all(8),
+              child: ListView.builder(
+                itemBuilder: (ctx, idx) {
+                  return _cardItem(lstMember[idx]);
+                },
+                itemCount: lstMember.length,
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+              ),
+            ),
           ),
-        ),
+          Positioned(
+            top: 10.0,
+            right: 1,
+            child: Container(
+              padding: const EdgeInsets.fromLTRB(24.0, 18.0, 24.0, 18.0),
+                child: Container(
+                  padding: const EdgeInsets.all(7.0),
+                    child: Icon(
+                      Icons.exit_to_app,
+                      color: Colors.white,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      shape: BoxShape.circle,
+                      boxShadow: [BoxShadow(
+                        color: Color.fromRGBO(0, 0, 0, 0.15),
+                        blurRadius: 8.0,
+                      )]
+                    ),
+                ),
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: Theme.of(context).primaryColor,
-        child: Icon(Icons.add, color: Colors.white, )
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
